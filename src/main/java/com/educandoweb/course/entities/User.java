a0926713f46f153aff7d4730fc6951a05e0f9ddd
andressaanunes <andressa_assuncao@live.com>
2022-02-15 15:41:20 -0300

    Prect created

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,100 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String passaword;
+	
+	public User() {
+	}
+
+	public User(long i, String name, String email, String phone, String passaword) {
+		super();
+		this.id = i;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.passaword = passaword;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassaword() {
+		return passaword;
+	}
+
+	public void setPassaword(String passaword) {
+		this.passaword = passaword;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
