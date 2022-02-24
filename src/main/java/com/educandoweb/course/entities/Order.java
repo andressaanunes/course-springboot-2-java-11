bf6bf75ef649f6331653a53da090c5dd0af607a7
andressaanunes <andressa_assuncao@live.com>
2022-02-24 13:13:23 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -47,10 +47,10 @@ public class Order implements Serializable {
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 
-   public Order() {
-	   
-   }
-	
+	public Order() {
+
+	}
+
 	public Order(Object object, Instant moment, OrderStatus orderStatus, User u1) {
 	}
 
@@ -107,6 +107,14 @@ public class Order implements Serializable {
 
 	public Set<OrderItem> getItems() {
 		return items;
+	}
+
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum = sum + x.getSubTotal();
+		}
+		return sum;
 	}
 
 	@Override
